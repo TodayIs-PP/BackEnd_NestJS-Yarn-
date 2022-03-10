48a282eb04d3f6b86ce664ebf70b5df41aa618b6
asdf148 <wnduf0405_@naver.com>
2022-03-10 18:44:24 +0900

    [CREATE] Create question module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FoodModule } from './food/food.module';
+import { QuestionModule } from './question/question.module';
 
 @Module({
-  imports: [FoodModule],
+  imports: [FoodModule, QuestionModule],
   controllers: [AppController],
   providers: [AppService],
 })
