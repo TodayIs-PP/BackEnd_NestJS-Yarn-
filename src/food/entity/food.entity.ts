efa1a34415ef62b60d4a47363140e7c18dc70bea
asdf148 <wnduf0405_@naver.com>
2022-03-10 17:00:00 +0900

    [FEAT] Add addFood function, food entity constructor

--- src/food/entity/food.entity.ts
+++ src/food/entity/food.entity.ts
@@ -1,10 +1,27 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { AddFood } from '../dto/addFood.dto';
 
 export type FoodDocument = Food & Document;
 
 @Schema()
 export class Food {
+  constructor(addFood?: AddFood) {
+    if (typeof addFood !== 'undefined') {
+      this.name = addFood.name;
+      this.image = addFood.image;
+      this.kind1 = addFood.kind1;
+      this.kind2 = addFood.kind2;
+      this.flavor1 = addFood.flavor1;
+      this.flavor2 = addFood.flavor2;
+    }
+    this.name = null;
+    this.image = null;
+    this.kind1 = null;
+    this.kind2 = null;
+    this.flavor1 = null;
+    this.flavor2 = null;
+  }
   id: string;
 
   @Prop()
--- src/food/food.service.ts
+++ src/food/food.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { AddFood } from './dto/addFood.dto';
+import { Food } from './entity/food.entity';
 import { FoodRepository } from './entity/food.repository';
 
 @Injectable()
 export class FoodService {
   constructor(private readonly foodRepository: FoodRepository) {}
 
-  getFoods(): Promise<any> {
-    return this.foodRepository.findAll();
+  async getFoods(): Promise<any> {
+    try {
+      return await this.foodRepository.findAll();
+    } catch (e) {
+      throw Error('Fail to load foods');
+    }
+  }
+
+  async addFood(addFood: AddFood): Promise<Food> {
+    const food = new Food(addFood);
+    try {
+      return await this.foodRepository.create(food);
+    } catch (e) {
+      throw Error('Fail to save foods');
+    }
   }
 }
