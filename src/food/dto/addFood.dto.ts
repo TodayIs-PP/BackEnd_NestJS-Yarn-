482db5164fadfd29a1f811370b64e2d767322c69
asdf148 <wnduf0405_@naver.com>
2022-03-12 21:39:04 +0900

    [TEST] Add addFood of Controller test code

--- src/food/dto/addFood.dto.ts
+++ src/food/dto/addFood.dto.ts
@@ -1,18 +1,18 @@
 export class AddFood {
   constructor(
-    name: string,
-    image: string,
-    kind1: string,
-    kind2: string,
-    flavor1: string,
+    name?: string,
+    image?: string,
+    kind1?: string,
+    kind2?: string,
+    flavor1?: string,
     flavor2?: string,
   ) {
-    this.name = name;
-    this.image = image;
-    this.kind1 = kind1;
-    this.kind2 = kind2;
-    this.flavor1 = flavor1;
-    this.flavor2 = flavor2;
+    this.name = name ?? null;
+    this.image = image ?? null;
+    this.kind1 = kind1 ?? null;
+    this.kind2 = kind2 ?? null;
+    this.flavor1 = flavor1 ?? null;
+    this.flavor2 = flavor2 ?? null;
   }
 
   name: string;
--- src/food/food.controller.spec.ts
+++ src/food/food.controller.spec.ts
@@ -1,18 +1,61 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { AddFood } from './dto/addFood.dto';
+import { Food } from './entity/food.entity';
 import { FoodController } from './food.controller';
+import { FoodService } from './food.service';
 
 describe('FoodController', () => {
   let controller: FoodController;
+  let service: FoodService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FoodController],
+      providers: [
+        {
+          provide: FoodService,
+          useValue: {
+            getFoods: jest.fn(),
+            addFood: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<FoodController>(FoodController);
+    service = module.get<FoodService>(FoodService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('addFood: Fail', async () => {
+    jest
+      .spyOn(service, 'addFood')
+      .mockRejectedValue(new Error('Fail to add food'));
+
+    try {
+      await controller.addFood(
+        new AddFood('소유라멘', null, '일식', '라멘', '짠맛', null),
+      );
+    } catch (e) {
+      expect(e).toBeInstanceOf(Error);
+      expect(e.message).toBe('Fail to add food');
+    }
+  });
+
+  it('addFood: Success', async () => {
+    jest
+      .spyOn(service, 'addFood')
+      .mockResolvedValue(
+        new Food('소유라멘', null, '일식', '라멘', '짠맛', null),
+      );
+
+    expect(
+      await controller.addFood(
+        new AddFood('소유라멘', null, '일식', '라멘', '짠맛', null),
+      ),
+    ).toBeInstanceOf(Food);
   });
 });
