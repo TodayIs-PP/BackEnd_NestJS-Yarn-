9f70d8e243b7c377b973bcabd740d50108b4d2ba
asdf148 <wnduf0405_@naver.com>
2022-03-13 10:44:36 +0900

    [FEAT] Change to accept image values as well

--- src/food/food.controller.ts
+++ src/food/food.controller.ts
@@ -1,5 +1,15 @@
-import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpStatus,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { Response } from 'express';
+import { multerDiskOptions } from '../global/multer.options';
 import { AddFood } from './dto/addFood.dto';
 import { FoodService } from './food.service';
 
@@ -8,12 +18,14 @@ export class FoodController {
   constructor(private readonly foodService: FoodService) {}
 
   @Post()
+  @UseInterceptors(FileInterceptor('image', multerDiskOptions))
   async addFood(
+    @UploadedFile() image: Express.Multer.File,
     @Body() addFood: AddFood,
     @Res() res: Response,
   ): Promise<Response<any, Record<string, any>>> {
     try {
-      await this.foodService.addFood(addFood);
+      await this.foodService.addFood(image, addFood);
       return res.status(HttpStatus.CREATED).json({ message: 'Food added' });
     } catch (e) {
       throw new Error(e.message);
