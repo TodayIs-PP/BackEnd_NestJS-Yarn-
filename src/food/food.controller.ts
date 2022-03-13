8ab9cbce6e72af3b5c7f96190746c997ec30ed11
asdf148 <wnduf0405_@naver.com>
2022-03-13 10:34:17 +0900

    [FEAT] Add addFood controller

--- src/food/food.controller.ts
+++ src/food/food.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { AddFood } from './dto/addFood.dto';
+import { FoodService } from './food.service';
 
 @Controller('food')
-export class FoodController {}
+export class FoodController {
+  constructor(private readonly foodService: FoodService) {}
+
+  @Post()
+  async addFood(
+    @Body() addFood: AddFood,
+    @Res() res: Response,
+  ): Promise<Response<any, Record<string, any>>> {
+    try {
+      await this.foodService.addFood(addFood);
+      return res.status(HttpStatus.CREATED).json({ message: 'Food added' });
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+}
