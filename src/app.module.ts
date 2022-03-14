4a41ffe1577e4b7c0d327d555c625a3c8ec77a6e
asdf148 <wnduf0405_@naver.com>
2022-03-14 09:59:29 +0900

    [FEAT] Add config file, MongoDB connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FoodModule } from './food/food.module';
 import { QuestionModule } from './question/question.module';
 
 @Module({
-  imports: [FoodModule, QuestionModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_URL),
+    FoodModule,
+    QuestionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
