7b809d623c064cdadc8ac829de364359e05e10e8
asdf148 <wnduf0405_@naver.com>
2022-03-16 10:30:09 +0900

    [FEAT, CREATE] Add GetCategories, Separate JsonResponse Type

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
-import { Response } from 'express';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ResponseDTO } from './global/response.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(@Res() res: Response) {
-    return res.status(HttpStatus.OK).json({ "test": this.appService.getHello(), 'test1': null });
+  @Get('categories')
+  getCategories(): ResponseDTO<string[]> {
+    return new ResponseDTO(null, this.appService.getCategories());
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,18 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getCategories(): Array<string> {
+    return [
+      '전체',
+      '한식',
+      '중식',
+      '양식',
+      '일식',
+      '분식',
+      '디저트',
+      '치킨',
+      '피자',
+      '패스트푸드',
+    ];
   }
 }
